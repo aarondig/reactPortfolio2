1facbb24be4ba9d5946ee227aaef52bf1a9f7d54
Aaron Diggdon <aarondiggdon@gmail.com>
2021-01-04 22:07:26 -0800

    added smooth scroll rig, tthen created a new ref to specify what shouldn't scroll

--- src/App.js
+++ src/App.js
@@ -1,14 +1,65 @@
-import React from "react";
-import Wrapper from "./components/Wrapper";
+import React, { useRef, useEffect } from "react";
+import useWindowSize from "./hooks/windowSize";
 import "./App.css";
+import Wrapper from "./components/Wrapper";
 import Landing from "./components/Landing";
 
 function App() {
+  const size = useWindowSize();
+
+  const app = useRef();
+  const scrollContainer = useRef();
+  const scrollable = useRef();
+
+  const data = {
+    ease: 0.1,
+    current: 0,
+    previous: 0,
+    rounded: 0,
+  };
+  useEffect(() => {
+    requestAnimationFrame(() => skewScrolling());
+  }, []);
+
+  useEffect(() => {
+    document.body.style.height = `${
+      scrollContainer.current.getBoundingClientRect().height
+    }px`;
+  }, [size.height]);
+
+
+// SCROLLING
+  const skewScrolling = () => {
+    //Set Current to the scroll position amount
+    data.current = window.scrollY;
+    // Set Previous to the scroll previous position
+    data.previous += (data.current - data.previous) * data.ease;
+      // Set rounded to
+    data.rounded = Math.round(data.previous * 100) / 100;
+
+    //VARIABLES
+    const difference = data.current - data.rounded;
+    const acceleration = difference / size.width;
+    const velocity = +acceleration;
+    const skew = velocity * 7.5;
+
+    //Assign skew and smooth scrolling to the scroll container
+    scrollable.current.style.transform = `translate3d(0, -${data.rounded}px, 0) skewY(${skew}deg)`;
+
+    requestAnimationFrame(() => skewScrolling());
+  };
   return (
-    <div className="App">
+    <div ref={app} className="App">
+      
       <Wrapper>
+        <div ref={scrollContainer} className="scroll">
         <Landing />
+        <div ref={scrollable}>
+        <div id="test">HI THERE</div>
+        </div>
+        </div>
       </Wrapper>
+      
     </div>
   );
 }
